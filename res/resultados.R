575b96f8a026a1d859918d1384ad500c8b6847b0
Debbie Con <debbie.con@ucr.ac.cr>
2025-07-11 14:19:13 -0600

    feat: resultados fisicos

--- res/resultados.R
+++ res/resultados.R
@@ -1,8 +1,9 @@
 
 library(readxl)
 
-tabla <- read_excel('historiales_pinn.xlsx')
 
+#historiales sin fisica 
+tabla <- read_excel('historiales_pinn.xlsx')
 
 
 lista_dfs <- split(tabla, seq(nrow(tabla)))
@@ -13,3 +14,21 @@ df.2 <- lista_dfs[[2]]
 df.3 <- lista_dfs[[3]]
 df.4 <- lista_dfs[[4]]
 df.5 <- lista_dfs[[5]]
+
+
+for (i in seq_along(lista_dfs)) {
+  write.csv(lista_dfs[[i]], paste0("df_", i, ".csv"), row.names = FALSE)
+}
+
+
+#historiales con fisica 
+
+tabla.f<- read.csv('historiales_con_Fisica.csv')
+
+lista_dfs_f <- split(tabla.f, seq(nrow(tabla.f)))
+
+for (i in seq_along(lista_dfs_f)) {
+  write.csv(lista_dfs_f[[i]], paste0("df_f_", i, ".csv"), row.names = FALSE)
+}
+
+
